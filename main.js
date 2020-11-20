45b4cbf37adb275955b7ef208eb3cb2ec00eaed6
edoardocollo <edoardo.collo@gmail.com>
2020-11-20 16:50:48 +0100

    comments

--- main.js
+++ main.js
@@ -3,6 +3,12 @@
 
 // VERIFICA SE UNA PAROLA È PALINDROMA
 /////////////////////////////////////////
+/**
+ * Verifica se una parola è palindroma.
+ *
+ * @param {string} x The number to raise.
+ * @return {boolean} x raised to the n-th power.
+ */
 
 
 function palindromi (parola){
@@ -30,43 +36,62 @@ if (palindromi(parolaUtente)){
 
 
 
-// // Pari e Dispari
-// //////////////////
-//
-//
-//
-// // L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
-// var pariODispari = prompt('scegli pari o dispari');
-// var numeroUtente = Number(prompt('scegli un numero da 1 a 5'));
-// console.log(pariODispari, numeroUtente);
-//
-// // Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
-// function random (){
-//
-//   var numero = Math.floor(Math.random() * 5  ) + 1 ;
-//   return numero;
-// }
-// var numeroPc = random();
-// console.log(numeroPc);
-//
-// // Sommiamo i due numeri
-//
-// var somma = numeroUtente + numeroPc;
-// console.log(somma);
-//
-// // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
-// function verificaPariDispari(numero){
-//   var verifica = true;
-//   if (numero % 2 !== 0){
-//     verifica = false;
-//   }
-//   return verifica;
-// }
-// console.log(verificaPariDispari(somma));
-// // Dichiariamo chi ha vinto.
-//
-// if (verificaPariDispari(somma)) {
-//   console.log('hai vinto');
-// }else{
-//   console.log('hai perso');
-// }
+// Pari e Dispari
+//////////////////
+
+
+
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+var pariODispari = prompt('scegli pari o dispari');
+var numeroUtente = Number(prompt('scegli un numero da 1 a 5'));
+console.log(pariODispari, numeroUtente);
+
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+
+
+
+/**
+ * Genera numero random.
+ *
+ * @return {number} numero random.
+ */
+
+function random (){
+
+  var numero = Math.floor(Math.random() * 5  ) + 1 ;
+  return numero;
+}
+var numeroPc = random();
+console.log(numeroPc);
+
+// Sommiamo i due numeri
+
+var somma = numeroUtente + numeroPc;
+console.log(somma);
+
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+
+
+
+/**
+ * Verifica se un numero è pari.
+ *
+ * @param {number}  numero da verificare.
+ * @return {boolean}
+ */
+
+function verificaPariDispari(numero){
+  var verifica = true;
+  if (numero % 2 !== 0){
+    verifica = false;
+  }
+  return verifica;
+}
+console.log(verificaPariDispari(somma));
+// Dichiariamo chi ha vinto.
+
+if (verificaPariDispari(somma)) {
+  console.log('hai vinto');
+}else{
+  console.log('hai perso');
+}
